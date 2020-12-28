3ad2967e62f9089c86ffdbdfac9c878a252f6a4e
Robert <robertjwayne@gmail.com>
2020-12-27 20:04:50 -0800

    Create TodoCustomUser controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './custom-user.controller';
 export * from './ping.controller';
 export * from './todo.controller';
 export * from './custom-user-todo.controller';
+export * from './todo-custom-user.controller';
---
+++ src/controllers/todo-custom-user.controller.ts
@@ -0,0 +1,38 @@
+import {
+  repository,
+} from '@loopback/repository';
+import {
+  param,
+  get,
+  getModelSchemaRef,
+} from '@loopback/rest';
+import {
+  Todo,
+  CustomUser,
+} from '../models';
+import {TodoRepository} from '../repositories';
+
+export class TodoCustomUserController {
+  constructor(
+    @repository(TodoRepository)
+    public todoRepository: TodoRepository,
+  ) { }
+
+  @get('/todos/{id}/custom-user', {
+    responses: {
+      '200': {
+        description: 'CustomUser belonging to Todo',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: getModelSchemaRef(CustomUser)},
+          },
+        },
+      },
+    },
+  })
+  async getCustomUser(
+    @param.path.string('id') id: typeof Todo.prototype.id,
+  ): Promise<CustomUser> {
+    return this.todoRepository.customUser(id);
+  }
+}
