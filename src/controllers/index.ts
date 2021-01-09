4206289de9d70b916dce8420660dc129e40e1bd4
Robert <robertjwayne@gmail.com>
2021-01-09 02:39:17 -0800

    Delete CustomUserTodo controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,4 +3,4 @@ export * from './ping.controller';
 export * from './project-todo.controller';
 export * from './project.controller';
 export * from './todo.controller';
-export * from './custom-user-todo.controller';
+
