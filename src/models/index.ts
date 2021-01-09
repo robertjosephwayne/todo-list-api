16bd2751a7e7321783472f2dcb70131ab3bff8d7
Robert <robertjwayne@gmail.com>
2021-01-09 02:37:14 -0800

    Create Project model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './custom-user.model';
+export * from './project.model';
 export * from './todo.model';
 
---
+++ src/models/project.model.ts
@@ -0,0 +1,35 @@
+import {belongsTo, Entity, hasMany, model, property} from '@loopback/repository';
+import {CustomUser} from './custom-user.model';
+import {Todo} from './todo.model';
+
+@model()
+export class Project extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @belongsTo(() => CustomUser)
+  customUserId: string;
+
+  @hasMany(() => Todo)
+  todos: Todo[];
+
+  constructor(data?: Partial<Project>) {
+    super(data);
+  }
+}
+
+export interface ProjectRelations {
+  // describe navigational properties here
+}
+
+export type ProjectWithRelations = Project & ProjectRelations;
